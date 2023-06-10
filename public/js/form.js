0e06cb03016e4a9dae362f7842737606ddb7dec0
Naoosky <yannicklemarchand1@gmail.com>
2023-06-10 14:01:35 +0200

    Ajout de la securiter du formulaire d'inscription et ajout de la securiter xss

---
+++ public/js/form.js
@@ -0,0 +1,45 @@
+let email = document.getElementById('email');
+let pseudo = document.getElementById('pseudo');
+let password = document.getElementById('password');
+let passwordConfirm = document.getElementById('confirmPassword');
+
+let emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}$/;
+
+
+
+if (email) {
+    email.addEventListener('keyup', () => {
+        if (emailRegex.test(email.value) === false) {
+            email.style.borderColor = 'red';
+        } else {
+            email.style.borderColor = 'green';
+        }
+    });
+}
+if (pseudo) {
+    pseudo.addEventListener('keyup', () => {
+        if (pseudo.value.length < 3) {
+            pseudo.style.borderColor = 'red';
+            e.preventDefault();
+        } else {
+            pseudo.style.borderColor = 'green';
+        }
+    });
+}
+if (password && passwordConfirm) {
+    password.addEventListener('keyup', () => {
+        if (password.value.length < 8) {
+            password.style.borderColor = 'red';
+        } else {
+            password.style.borderColor = 'green';
+        }
+    });
+
+    passwordConfirm.addEventListener('keyup', () => {
+        if (password.value !== passwordConfirm.value) {
+            passwordConfirm.style.borderColor = 'red';
+        } else {
+            passwordConfirm.style.borderColor = 'green';
+        }
+    });
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,16 @@
 'use strict';
 
-let dyslexia = false;
-let dyslexiaBtn = document.getElementById('dyslexia');
+document.addEventListener('DOMContentLoaded', () => {
+    let dyslexia = false;
+    let dyslexiaBtn = document.getElementById('dyslexia');
 
-dyslexiaBtn.addEventListener('click', () => {
-    dyslexia = !dyslexia;
-    dyslexiaBtn.classList.toggle('active');
-    if (dyslexia) {
-        document.body.classList.add('dyslexia');
-    } else {
-        document.body.classList.remove('dyslexia');
-    }
+    dyslexiaBtn.addEventListener('click', () => {
+        dyslexia = !dyslexia;
+        dyslexiaBtn.classList.toggle('active');
+        if (dyslexia) {
+            document.body.classList.add('dyslexia');
+        } else {
+            document.body.classList.remove('dyslexia');
+        }
+    });
 });
