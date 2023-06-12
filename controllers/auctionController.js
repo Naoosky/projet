e07b92e6566b2acdace1be6a52ef3c4e34c6fe4d
Naoosky <yannicklemarchand1@gmail.com>
2023-06-12 17:29:59 +0200

    ajout formulaire items

--- controllers/auctionController.js
+++ controllers/auctionController.js
@@ -1,7 +1,7 @@
 import pool from "../config/database.js";
 
 export const auction = (req, res) => {
-    let sql = " SELECT * FROM items";
+    let sql = " SELECT * FROM items INNER JOIN images ON image_id = images.id";
     pool.query(sql, function (error, result) {
         if (error) {
             console.error(error)
@@ -13,30 +13,50 @@ export const auction = (req, res) => {
 }
 
 export const addItems = (req, res) => {
+    let id = req.session.userId;
+
     let sql = "SELECT * FROM images";
-    pool.query(sql, function (error, result) {
+    pool.query(sql, function (error, images) {
         if (error) {
             console.error(error)
             res.status(500).send('erreur de bdd')
         } else {
-            res.render('layout', {template: 'addItems', images: result});
+            let query = "SELECT * FROM users WHERE id = ?";
+            pool.query(query, id, function (error, user) {
+                if (error) {
+                    console.error(error)
+                    res.status(500).send('erreur de bdd')
+                } else {
+                    let sql2 = "SELECT * FROM category_items";
+                    pool.query(sql2, function (error, category) {
+                        if (error) {
+                            console.error(error)
+                            res.status(500).send('erreur de bdd')
+                        } else {
+                            res.render('layout', {template: 'addItems', images: images, user: user[0], category: category});
+                        }
+                    });
+                }
+            });
         }
     });
 }
 
 export const addItemsSubmit = (req, res) => {
     let id = req.session.userId
+    console.log(req.body)
     let newItems = {
 
     }
-    let sql = "INSERT INTO items SET ?";
-    pool.query(sql, [newItems], function (error, result) {
-        if (error) {
-            console.error(error)
-            res.status(500).send('erreur de bdd')
-        } else {
-            res.redirect('/profile'+id)
-        }
-    });
+    res.redirect('/profile/'+id)
+    // let sql = "INSERT INTO items SET ?";
+    // pool.query(sql, [newItems], function (error, result) {
+    //     if (error) {
+    //         console.error(error)
+    //         res.status(500).send('erreur de bdd')
+    //     } else {
+    //         res.redirect('/profile'+id)
+    //     }
+    // });
 
 }
