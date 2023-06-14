1a8df836c941b8e786f75cae1ed878a103af7c6b
Naoosky <yannicklemarchand1@gmail.com>
2023-06-14 09:34:14 +0200

    Moddif commentaire / formulaire

--- controllers/forumController.js
+++ controllers/forumController.js
@@ -1,62 +1,100 @@
 import pool from "../config/database.js";
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidV4} from 'uuid';
 
 
 export const forum = (req, res) => {
     res.render('layout', {template: 'forum'});
 }
 
-export const articlesDetails = (req,res) => {
+export const articlesDetails = (req, res) => {
     const id = req.params.id
     let sql = `
-    SELECT articles.id, articles.title, articles.description, articles.date, category_articles.name as 'category', comments.pseudo, comments.comment, comments.date as 'dateComments'
-      FROM articles
-           INNER JOIN category_articles ON articles.category_id = category_articles.id
-           LEFT JOIN comments ON comments.article_id  = articles.id 
-     WHERE articles.id = ?
-           
+        SELECT articles.id,
+               articles.title,
+               articles.description,
+               articles.date,
+               category_articles.name as 'category',
+               comments.pseudo,
+               comments.comment,
+               comments.date          as 'dateComments'
+        FROM articles
+                 INNER JOIN category_articles ON articles.category_id = category_articles.id
+                 LEFT JOIN comments ON comments.article_id = articles.id
+        WHERE articles.id = ?
+
     `;
-    pool.query(sql,id, (error,results) => {
-        if(error){
+    pool.query(sql, id, (error, results) => {
+        if (error) {
             console.error(error)
-        }else{
+        } else {
 
             res.render('layout', {template: 'articles', articles: results})
         }
     })
 }
 
-export const addComments = (req,res) => {
+export const addComments = (req, res) => {
     const id = req.params.id
 
-    const newComment = {
-        id: uuidv4(),
-        pseudo: req.body.pseudo,
-        comment: req.body.comment,
-        article_id: id
-    }
-    let sql = `INSERT INTO comments SET ?`;
-    pool.query(sql,newComment, (error,results) =>{
-        if(error){
-            console.error(error)
-        }else{
-            res.redirect('/articles/'+id)
+    if (req.session.isUser) {
+
+        const userId = req.session.userId
+        const sql = 'SELECT * FROM users WHERE id = ?';
+
+        pool.query(sql, userId, (error, user) => {
+            if (error) {
+                console.error(error)
+            } else {
+                const newCommentUser = {
+                    id: uuidV4(),
+                    pseudo: user[0].pseudo,
+                    comment: req.body.comment,
+                    article_id: id
+                }
+
+                let sql = 'INSERT INTO comments SET ?';
+
+                pool.query(sql, newCommentUser, (error) => {
+                    if (error) {
+                        console.error(error)
+                    } else {
+                        res.redirect('/articles/' + id)
+                    }
+                });
+            }
+        });
+
+    } else {
+
+        const newComment = {
+            id: uuidV4(),
+            pseudo: req.body.pseudo,
+            comment: req.body.comment,
+            article_id: id
         }
-    })
+        let sql2 = "INSERT INTO comments SET  ?";
+        pool.query(sql2, newComment, (error) => {
+            if (error) {
+                console.error(error)
+            } else {
+                res.redirect('/articles/' + id)
+            }
+        })
+    }
 }
 
-export const addArticles = (req,res) => {
+export const addArticles = (req, res) => {
     let id = req.params.id
-    const query = `SELECT * FROM category_articles`;
-    pool.query(query, (error,category) => {
-        if(error){
+    const query = 'SELECT * FROM category_articles';
+    pool.query(query, (error, category) => {
+        if (error) {
             console.error(error)
-        }else{
-            const sql = `SELECT * FROM users WHERE id = ?`;
-            pool.query(sql,id, (error,user) => {
-                if(error){
+        } else {
+            const sql = 'SELECT * FROM users WHERE id = ?';
+            pool.query(sql, id, (error, user) => {
+                if (error) {
                     console.error(error)
-                }else{
+                } else {
                     res.render('layout', {template: 'addArticles', category: category, user: user[0]});
                 }
             });
@@ -64,21 +102,22 @@ export const addArticles = (req,res) => {
     });
 }
 
-export const addArticlesSubmit = (req,res) => {
+export const addArticlesSubmit = (req, res) => {
     const id = req.session.userId
     const newArticle = {
-        id: uuidv4(),
+        id: uuidV4(),
         title: req.body.title,
         description: req.body.description,
         category_id: req.body.category,
         user_id: req.body.user
     }
-    let sql = `INSERT INTO articles SET ?`;
-    pool.query(sql,newArticle, (error,results) =>{
-        if(error){
+    let sql = `INSERT INTO articles
+               SET ?`;
+    pool.query(sql, newArticle, (error) => {
+        if (error) {
             console.error(error)
-        }else{
-            res.redirect('/profile/'+id)
+        } else {
+            res.redirect('/profile/' + id)
         }
     })
 }
