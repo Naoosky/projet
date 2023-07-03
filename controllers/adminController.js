8013c08b218cdfe1aa6086411f0abe9350f311e6
Naoosky <yannicklemarchand1@gmail.com>
2023-07-03 20:42:12 +0200

    Modif commentaire

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,6 @@
 import pool from "../config/database.js";
 
-// Page d'administration
+// Page d’administration
 export const admin = (req, res) => {
     if (!req.session.isAdmin) {
         res.redirect('/');
@@ -33,27 +33,27 @@ export const usersProfil = (req, res) => {
         res.redirect('/');
     } else {
         const id = req.params.id;
-        // Requête pour récupérer les informations de l'utilisateur
+        // Requête pour récupérer les informations de l’utilisateur
         let query = "SELECT id, pseudo FROM users WHERE id = ?";
         pool.query(query, [id], function (error, result) {
             if (error) {
                 console.error(error);
                 res.status(500).send('erreur de bdd');
             } else {
-                // Requête pour récupérer les articles de l'utilisateur
+                // Requête pour récupérer les articles de l’utilisateur
                 const query = "SELECT id, title, description FROM articles WHERE user_id = ?";
                 pool.query(query, [id], (error, articles) => {
                     if (error) {
                         console.error(error);
                     } else {
-                        // Requête pour récupérer les items de l'utilisateur avec les images correspondantes
+                        // Requête pour récupérer les items de l’utilisateur avec les images correspondantes
                         let sql = "SELECT items.id, items.title, items.content, items.price, images.url, images.description FROM items INNER JOIN images ON image_id = images.id WHERE user_id = ?";
                         pool.query(sql, [id], function (error, items) {
                             if (error) {
                                 console.error(error);
                                 res.status(500).send('erreur de bdd');
                             } else {
-                                // Rendu de la page de profil de l'utilisateur avec ses informations, articles et items
+                                // Rendu de la page de profil de l’utilisateur avec ses informations, articles et items
                                 res.render('layout', {
                                     template: 'usersProfil',
                                     user: result[0],
@@ -74,7 +74,7 @@ export const deleteUser = (req, res) => {
     const id = req.params.id;
     const userId = req.session.userId;
 
-    // Vérification si l'utilisateur supprime son propre compte
+    // Vérification si l’utilisateur supprime son propre compte
     if (userId === id) {
         let sql = `DELETE
                    FROM users
@@ -91,7 +91,7 @@ export const deleteUser = (req, res) => {
             }
         });
     }
-    // Vérification si l'utilisateur est administrateur et supprime le compte d'un autre utilisateur
+    // Vérification si l’utilisateur est administrateur et supprime le compte d’un autre utilisateur
     else if (req.session.isAdmin) {
         let sql = `DELETE
                    FROM users
@@ -107,7 +107,7 @@ export const deleteUser = (req, res) => {
             }
         });
     }
-    // L'utilisateur n'est pas autorisé à supprimer le compte
+    // L’utilisateur n’est pas autorisé à supprimer le compte
     else {
         res.status(403).send('Forbidden');
     }
